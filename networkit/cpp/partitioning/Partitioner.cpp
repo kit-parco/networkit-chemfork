616978e0e96477b733a6187c07d43f834904f94e
Moritz von Looz <moritz.looz-corswarem@kit.edu>
2015-10-13 14:58:34 +0200

    oh well, it compiles - extended partitioning stub

--- networkit/cpp/partitioning/Partitioner.cpp
+++ networkit/cpp/partitioning/Partitioner.cpp
@@ -5,7 +5,14 @@
  *      Author: moritzl
  */
 
+#include <tuple>
+
 #include "Partitioner.h"
+#include "../auxiliary/PrioQueue.h"
+
+using std::pair;
+using std::vector;
+using Aux::PrioQueue;
 
 namespace NetworKit {
 namespace GraphTools {
@@ -55,8 +62,97 @@ void Partitioner::run() {
 	hasRun = true;
 }
 
-void Partitioner::fiducciaMatheyses(const Graph& g, Partition& input) {
+void Partitioner::fiducciaMatheyses(const Graph& g, Partition&  part) {
+	/**
+	 * necessary data structures:
+	 * - integer priority queue for each partition
+	 * - history of move operations and gain values
+	 */
 
+	/**
+	 * allocate data structures
+	 */
+	const count k = part.numberOfSubsets();
+	const count n = part.numberOfElements();
+	vector<PrioQueue<edgeweight, index> > queues(k,0);
+	vector<edgeweight> gains;
+	vector<pair<index, index> > transfers;
+	vector<index> transferedVertices;
+	edgeweight total = g.totalEdgeWeight();
+
+	/**
+	 * fill priority queues for the first time
+	 */
+	for (index ID : part.getSubsetIds()) {
+		for (index j : part.getMembers(ID)) {
+			edgeweight maxGain = -total;
+			index IdAtMax = 0;
+			for (index otherSubset : part.getSubsetIds()) {
+				edgeweight thisgain = calculateGain(g, part, j, otherSubset);
+				if (thisgain > maxGain && otherSubset != ID) {
+					IdAtMax = otherSubset;
+					maxGain = thisgain;
+				}
+			}
+			assert(ID < queues.size());
+			queues[ID].insert(maxGain, j);
+		}
+	}
+
+	/**
+	 * do one step
+	 */
+	bool allQueuesEmpty = false;
+	while (!allQueuesEmpty) {
+		allQueuesEmpty = true;
+		for (index partID : part.getSubsetIds()) {
+			assert(partID < queues.size());
+			index topVertex;
+			double topGain;
+			std::tie(topGain, topVertex) = queues[partID].extractMin();
+
+			//now get target partition. This could be sped up by saving the target partition in a separate data structure
+			edgeweight maxGain = -total;
+			index IdAtMax = 0;
+			for (index otherSubset : part.getSubsetIds()) {
+				edgeweight thisgain = calculateGain(g, part, topVertex, otherSubset);
+				if (thisgain > maxGain && otherSubset != partID) {
+					IdAtMax = otherSubset;
+					maxGain = thisgain;
+				}
+			}
+
+			//move node there
+			part.moveToSubset(IdAtMax, topVertex);
+
+			//update history
+			gains.push_back(maxGain);
+			transfers.emplace_back(partID, IdAtMax);
+			transferedVertices.push_back(topVertex);
+
+			//update gains of neighbours
+			g.forNeighborsOf(topVertex, [&g, &topVertex, &partID, &total, &queues, &part](index w){
+				//update gain
+				edgeweight newMaxGain = -total;
+				for (index otherSubset : part.getSubsetIds()) {
+					edgeweight thisgain = calculateGain(g, part, topVertex, otherSubset);
+					if (thisgain > newMaxGain && otherSubset != partID) {
+						newMaxGain = thisgain;
+					}
+				}
+
+				//update prioqueue
+				queues[part[w]].remove(w);
+				queues[part[w]].insert(newMaxGain, w);
+			});
+
+			allQueuesEmpty = allQueuesEmpty && queues[partID].size() == 0;
+		}
+	}
+}
+
+edgeweight Partitioner::calculateGain(const Graph& g, const Partition& input, index v, index targetPart) {
+	return 0;
 }
 
 Partition Partitioner::getPartition() {
