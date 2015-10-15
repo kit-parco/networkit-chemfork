96d77a40640dfdb8da5d007a0198ca3be596ce95
Moritz von Looz <moritz.looz-corswarem@kit.edu>
2015-10-15 13:53:01 +0200

    added stub of test case class

---
+++ networkit/cpp/partitioning/test/PartitionerGTest.h
@@ -0,0 +1,23 @@
+/*
+ * PartitionerGTest.h
+ *
+ *  Created on: 15.10.2015
+ *      Author: moritzl
+ */
+
+#ifndef NOGTEST
+
+#ifndef PARTITIONERGTEST_H_
+#define PARTITIONERGTEST_H_
+
+
+#include <gtest/gtest.h>
+
+
+class PartitionerGTest: public testing::Test  {
+
+};
+
+#endif /* PARTITIONERGTEST_H_ */
+
+#endif /*NOGTEST */
