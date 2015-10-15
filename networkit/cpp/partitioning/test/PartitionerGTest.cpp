89120c929c73ac39b8f24d8140fde6e2da62fee2
Moritz von Looz <moritz.looz-corswarem@kit.edu>
2015-10-15 17:25:56 +0200

    added test case for graph partitioner exposed bug in LocalMaxMatcher

--- networkit/cpp/partitioning/test/PartitionerGTest.cpp
+++ networkit/cpp/partitioning/test/PartitionerGTest.cpp
@@ -9,17 +9,39 @@
 
 #include "../Partitioner.h"
 #include "../../graph/Graph.h"
+#include "../../Globals.h"
+#include "../../generators/BarabasiAlbertGenerator.h"
 
-using namespace NetworKit;
+namespace NetworKit {
 
-TEST_F(PartitionerGTest, testGain) {
+TEST_F(PartitionerGTest, testGainUnweighted) {
 	Graph G(6);
-	G.addEdge(1,2);
-	G.addEdge(1,2);
-	G.addEdge(1,2);
-	G.addEdge(1,2);
-	G.addEdge(1,2);
-	G.addEdge(1,2);
+	G.addEdge(0,1);
+	G.addEdge(0,2);
+	G.addEdge(1,3);
+	G.addEdge(2,3);
+	G.addEdge(1,4);
+	G.addEdge(3,5);
+	G.addEdge(4,5);
+	G.addEdge(3,4);
 
+	Partition part(6);
+	part.allToOnePartition();
+	part.setUpperBound(2);
+	index secondPartition = 1;
+	part.moveToSubset(secondPartition, 4);
+	part.moveToSubset(secondPartition, 5);
+
+	EXPECT_EQ(0, Partitioner::calculateGain(G, part, 3, secondPartition));
+	EXPECT_EQ(-1, Partitioner::calculateGain(G, part, 1, secondPartition));
 }
 
+TEST_F(PartitionerGTest, testPartitioner) {
+	BarabasiAlbertGenerator gen(10, 1000, 10);
+
+	Graph G = gen.generate();
+	Partitioner part(G, 10);
+	part.run();
+}
+
+}//end namespace NetworKit
